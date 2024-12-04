2b783f52e6520f3ba59b9c0e4692d3cff5d04f43
Raul <raulferras@gmail.com>
2024-12-04 12:31:36 +0100

    Install / Update automatically versions of LSP (#8)
    * Split extension code different files.
    Implement a LSP version check + download + install.
    
    * Fix activateLS not waiting for setupC3 to finish.
    
    * Set default version number
    
    * Remove build extension.js
    
    * Remove unzipper dependency.

---
+++ src/setupC3.js
@@ -0,0 +1,45 @@
+import vscode from "vscode";
+import { installLSP } from "./lsp";
+
+export async function setupC3(context) {
+    await initialSetup(context)
+}
+
+async function initialSetup(context) {
+    const lsConfig = vscode.workspace.getConfiguration("c3.lsp");
+
+    if (!lsConfig.get("path")) {
+        const response = await vscode.window.showInformationMessage(
+            "You can enable C3LSP (the C3 Language Server) for a better editing experience. Would you like to install it?",
+            { modal: true },
+            "Install",
+            "Specify path",
+            "Use C3LSP in PATH",
+        );
+
+        switch (response) {
+            case "Install":
+                await installLSP(context)
+                break;
+            case "Specify path":
+                const uris = await vscode.window.showOpenDialog({
+                    canSelectFiles: true,
+                    canSelectFolders: false,
+                    canSelectMany: false,
+                    title: "Select C3 Language Server (C3LSP) executable",
+                });
+                if (!uris) return true;
+
+                await lsConfig.update("path", uris[0].path, true);
+                break;
+            
+            case "Use C3LSP in PATH":
+                await lsConfig.update("path", "c3lsp", true);
+                break;
+            case undefined:
+                break;
+        }
+    }
+
+    return true;
+}
