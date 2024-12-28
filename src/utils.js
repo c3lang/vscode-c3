3f7f312a120387ca2e6ff8e7eb17f07ce071a4d9
tonis2 <tonis.anton18@gmail.com>
2024-12-28 03:14:50 -0800

    Fix windows lsp install

--- src/utils.js
+++ src/utils.js
@@ -8,67 +8,60 @@ import vscode from "vscode";
 const chmod = promisify(fs.chmod);
 
 export async function downloadAndExtractArtifact(
-    /** e.g. `C3CLSP` */
-    title,
-    /** e.g. `c3lsp` */
-    executableName,
-    /** e.g. inside `context.globalStorageUri` */
-    installDir,
-    artifactUrl,
-    /** The expected sha256 hash (in hex) of the artifact/tarball. */
-    //sha256,
-    /** Extract arguments that should be passed to `tar`. e.g. `--strip-components=1` */
-    extraTarArgs ,
+	/** e.g. `C3CLSP` */
+	title,
+	/** e.g. `c3lsp` */
+	executableName,
+	/** e.g. inside `context.globalStorageUri` */
+	installDir,
+	artifactUrl,
+	/** The expected sha256 hash (in hex) of the artifact/tarball. */
+	//sha256,
+	/** Extract arguments that should be passed to `tar`. e.g. `--strip-components=1` */
+	extraTarArgs,
 ) {
-    return await vscode.window.withProgress(
-        {
-            title: `Installing ${title}`,
-            location: vscode.ProgressLocation.Notification,
-        },
-        async (progress) => {
-            progress.report({ message: `downloading ${title} zip...` });
-            const response = await axios.get(artifactUrl, {
-                responseType: "arraybuffer",
-                onDownloadProgress: (progressEvent) => {
-                    if (progressEvent.total) {
-                        const increment = (progressEvent.bytes / progressEvent.total) * 100;
-                        progress.report({
-                            message: progressEvent.progress
-                                ? `downloading zip ${(progressEvent.progress * 100).toFixed()}%`
-                                : "downloading zip...",
-                            increment: increment,
-                        });
-                    }
-                },
-            });
+	return await vscode.window.withProgress(
+		{
+			title: `Installing ${title}`,
+			location: vscode.ProgressLocation.Notification,
+		},
+		async (progress) => {
+			progress.report({ message: `downloading ${title} zip...` });
+			const response = await axios.get(artifactUrl, {
+				responseType: "arraybuffer",
+				onDownloadProgress: (progressEvent) => {
+					if (progressEvent.total) {
+						const increment = (progressEvent.bytes / progressEvent.total) * 100;
+						progress.report({
+							message: progressEvent.progress ? `downloading zip ${(progressEvent.progress * 100).toFixed()}%` : "downloading zip...",
+							increment: increment,
+						});
+					}
+				},
+			});
 
-            const zipUri = vscode.Uri.joinPath(installDir, path.basename(artifactUrl));
+			const zipUri = vscode.Uri.joinPath(installDir, path.basename(artifactUrl));
+			const isWindows = process.platform === "win32";
+			// Delete old lsp folder
+			await vscode.workspace.fs.delete(installDir, { recursive: true, useTrash: false }).catch((err) => {});
+			await vscode.workspace.fs.createDirectory(installDir);
+			await vscode.workspace.fs.writeFile(zipUri, response.data);
 
-            // Delete old lsp folder
-            await vscode.workspace.fs.delete(installDir, { recursive: true, useTrash: false }).catch((err) => {})
-            await vscode.workspace.fs.createDirectory(installDir);
-            await vscode.workspace.fs.writeFile(zipUri, response.data);
-            
-            progress.report({ message: "Extracting..." });
-            const files = await decompress(zipUri.path, installDir.path, {
-                map: file => {
-                    const isWindows = process.platform === "win32";
-                    file.path = `${file.path}${isWindows ? ".exe" : ""}`;
-                    return file;
-                }
-            });
-            
-            const exePath = vscode.Uri.joinPath(installDir, files[0].path).fsPath;
+			progress.report({ message: "Extracting..." });
+			const zip_path = isWindows ? zipUri.path.slice(1) : zipUri.path;
+			const install_path = isWindows ? installDir.path.slice(1) : installDir.path;
 
-            // await fs.promises.rename(exePathDeprecated, exePath, (err) => {
-            //     if (err) {
-            //         void vscode.window.showErrorMessage(`Error renaming file: ${err}`);
-            //     }
-            // });
+			const files = await decompress(zip_path, install_path, {
+				map: (file) => {
+					file.path = file.path;
+					return file;
+				},
+			});
 
-            await chmod(exePath, 0o755);
+			const exePath = vscode.Uri.joinPath(installDir, files[0].path).fsPath;
+			await chmod(exePath, 0o755);
 
-            return exePath;
-        },
-    );
+			return exePath;
+		},
+	);
 }
