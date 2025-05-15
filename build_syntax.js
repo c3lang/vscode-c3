60859dc1f0fb3b4cf4c0abe66b3960693b864235
Christian Buttner <christian@cbuttner.de>
2025-05-15 15:17:16 +0200

    TM: Update keywords.

--- build_syntax.js
+++ build_syntax.js
@@ -15,12 +15,12 @@ const variables = {
   "CONST": '(?:_*[A-Z][_A-Z0-9]*)',
   "TYPE":  '(?:_*[A-Z][_A-Z0-9]*[a-z][_a-zA-Z0-9]*)',
   "IDENT": '(?:_*[a-z][_a-zA-Z0-9]*)',
-  "keyword": 'assert|asm|catch|const|alias|typedef|extern|tlocal|inline|import|module|interface|static|try|var',
+  "keyword": 'alias|assert|asm|attrdef|catch|const|extern|faultdef|tlocal|inline|import|module|interface|static|try|typedef|var',
   "control_keyword": 'break|case|continue|default|defer|do|else|for|foreach|foreach_r|if|nextcase|return|switch|while',
-  "ct_keyword": 'alignof|and|append|assert|assignable|concat|default|defined|echo|embed|eval|error|exec|extnameof|feature|include|is_const|nameof|offsetof|or|qnameof|sizeof|stringify|vacount|vaconst|varef|vaarg|vaexpr|vasplat',
+  "ct_keyword": 'alignof|assert|assignable|default|defined|echo|embed|eval|evaltype|error|exec|extnameof|feature|include|is_const|nameof|offsetof|qnameof|sizeof|stringify|typefrom|typeof|vacount|vatype|vaconst|vaarg|vaexpr|vasplat',
   "ct_control_keyword": 'case|else|endfor|endforeach|endif|endswitch|for|foreach|if|switch',
   "base_type": 'void|bool|char|double|float|float16|int128|ichar|int|iptr|isz|long|short|uint128|uint|ulong|uptr|ushort|usz|float128|any|anyfault|typeid',
-  "attribute": 'align|benchmark|bigendian|builtin|callconv|compact|const|deprecated|dynamic|export|extern|finalizer|if|inline|init|link|littleendian|local|maydiscard|naked|nodiscard|noinit|noinline|nopadding|noreturn|nosanitize|nostrip|obfuscate|operator|optional|overlap|packed|private|public|pure|reflect|safemacro|section|test|unused|used|wasm|weak|winmain',
+  "attribute": 'align|benchmark|bigendian|builtin|callconv|compact|const|deprecated|dynamic|export|extern|finalizer|format|if|inline|init|link|littleendian|local|maydiscard|naked|noalias|nodiscard|noinit|noinline|nopadding|norecurse|noreturn|nosanitize|nostrip|obfuscate|operator|operator_r|operator_s|optional|overlap|packed|private|public|pure|reflect|safemacro|section|tag|test|unused|used|wasm|weak|winmain',
 };
 
 text = text.replaceAll(/\{\{([A-Za-z0-9_]+)\}\}/g, (_, x) => variables[x]);
