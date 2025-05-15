0b3eeda0e8a17a1c12d7c5b7c906be3867bb892e
Christian Buttner <christian@cbuttner.de>
2025-05-15 22:39:11 +0200

    TM: Refactor const declarations, fix CT type parameters

--- build_syntax.js
+++ build_syntax.js
@@ -15,7 +15,7 @@ const variables = {
   "CONST": '(?:_*[A-Z][_A-Z0-9]*)',
   "TYPE":  '(?:_*[A-Z][_A-Z0-9]*[a-z][_a-zA-Z0-9]*)',
   "IDENT": '(?:_*[a-z][_a-zA-Z0-9]*)',
-  "keyword": 'assert|asm|catch|const|extern|tlocal|inline|import|module|interface|static|try|var',
+  "keyword": 'assert|asm|catch|extern|tlocal|inline|import|module|interface|static|try|var',
   "control_keyword": 'break|case|continue|default|defer|do|else|for|foreach|foreach_r|if|nextcase|return|switch|while',
   "ct_keyword": 'alignof|assert|assignable|default|defined|echo|embed|eval|evaltype|error|exec|extnameof|feature|include|is_const|nameof|offsetof|qnameof|sizeof|stringify|typefrom|typeof|vacount|vatype|vaconst|vaarg|vaexpr|vasplat',
   "ct_control_keyword": 'case|else|endfor|endforeach|endif|endswitch|for|foreach|if|switch',
